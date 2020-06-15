7f404c5b0d2e0ddd537cac88badbdd72754882b0
caisi <caisi1735@163.com>
2020-06-15 09:45:54 +0800

    改为链接echarts的js库

--- covid_19_dv/app/views.py
+++ covid_19_dv/app/views.py
@@ -6,7 +6,7 @@ from pyecharts_javascripthon.api import TRANSLATOR
 
 # Create your views here.
 REMOTE_HOST = "./static/js/map/province"
-HOST = "./static/js/assets/js"
+HOST = "https://pyecharts.github.io/assets/js"
 
 
 def index(request):
