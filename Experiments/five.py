79b2bbc42d5e2e7b76da5fa6e818bf90fd5a4501
caisi <caisi1735@163.com>
2020-06-15 09:31:20 +0800

    实验

--- Experiments/five.py
+++ Experiments/five.py
@@ -2,74 +2,64 @@ import pandas as pd
 from pyecharts import Radar, Boxplot
 
 
-def nien_to_nan(df, columns):
+def nien_to_nan(df, columns_hcs):
     """
         将最大值的噪声数据设为nan
     :param df:      dataframe
-    :param columns: 需要转的列
+    :param columns_hcs: 需要转的列
     :return:        新的dataframe
     """
-    for column in columns:
+    for column_hcs in columns_hcs:
         # 获取最大值
-        max = df.loc[:, column].max()
+        max_hcs = df.loc[:, column_hcs].max()
         # 如果是未知噪声数据
-        if max == 99 or max == 999 or max == 9999:
+        if max_hcs == 99 or max_hcs == 999 or max_hcs == 9999:
             # 获取存在噪声数据的所有行， 类型是一个dataframe
-            birth_nien = df[df[column].isin([max])]
+            birth_nien_hcs = df[df[column_hcs].isin([max_hcs])]
             # 遍历噪声数据行的index
-            for index in birth_nien.index:
+            for index_hcs in birth_nien_hcs.index:
                 # 将传入的dataframe中的噪声数据设为nan
-                df.loc[index, column] = None
+                df.loc[index_hcs, column_hcs] = None
     return df
 
 
-def get_data(file_path):
+def picture_radar(file_path):
     """
     :param file_path:文件路径：str
-    :return: tuple:存活婴儿、死亡婴儿中五个字段的平均值，类型为list
     """
-    birth = pd.read_csv(file_path)
+    birth_hcs = pd.read_csv(file_path)
     # 使用loc 进行切片， 行：全选  列：选择不连续的用列表包含列名
-    birth_nien = birth.loc[:, ['INFANT_ALIVE_AT_REPORT',
+    birth_nien_hcs = birth_hcs.loc[:, ['INFANT_ALIVE_AT_REPORT',
                                'MOTHER_AGE_YEARS',
                                'CIG_1_TRI',
                                'MOTHER_PRE_WEIGHT',
                                'OBSTETRIC_GESTATION_WEEKS',
                                'INFANT_WEIGHT_GRAMS']]
 
-    birth_nien = nien_to_nan(birth_nien,
+    birth_nien_hcs = nien_to_nan(birth_nien_hcs,
                              ['MOTHER_AGE_YEARS', 'CIG_1_TRI', 'MOTHER_PRE_WEIGHT', 'OBSTETRIC_GESTATION_WEEKS',
                               'INFANT_WEIGHT_GRAMS'])
 
     # birth_nien['INFANT_ALIVE_AT_REPORT'] == 'Y'   判断row的值：等于则为True  再使用DataFrame取为True的行
-    y = birth_nien[birth_nien['INFANT_ALIVE_AT_REPORT'] == 'Y']
-    n = birth_nien[birth_nien['INFANT_ALIVE_AT_REPORT'] == 'N']
+    y_hcs = birth_nien_hcs[birth_nien_hcs['INFANT_ALIVE_AT_REPORT'] == 'Y']
+    n_hcs = birth_nien_hcs[birth_nien_hcs['INFANT_ALIVE_AT_REPORT'] == 'N']
     # 按列求平均值，跳过nan，
     # round(decimals=2)： 四舍五入取两位小数
-    y_mean = list(y.mean(skipna=True, axis=0).round(decimals=2))
-    n_mean = list(n.mean(skipna=True, axis=0).round(decimals=2))
-    return y_mean, n_mean
-
-
-def picture_radar(y_list, n_list):
-    """
-        绘制雷达图
-    :param y_list: 存活婴儿的列平均值列表
-    :param n_list: 死亡婴儿的列平均值列表
-
-    """
-    schema = [{'name': "母亲平均年龄", 'max': 40},
+    y_mean_hcs = list(y_hcs.mean(skipna=True, axis=0).round(decimals=2))
+    n_mean_hcs = list(n_hcs.mean(skipna=True, axis=0).round(decimals=2))
+    print('存活婴儿数据雷达图:', y_mean_hcs, '\n', '死亡婴儿数据雷达图:', n_mean_hcs)
+    schema_hcs = [{'name': "母亲平均年龄", 'max': 40},
               {'name': "母亲平均吸烟数量", 'max': 1.2},
               {'name': "母亲平均体重", 'max': 200},
               {'name': "怀孕平均周数", 'max': 40},
               {'name': "婴儿平均体重", 'max': 3500}]
-    radar = Radar('存活婴儿与死亡婴儿数据对比——雷达图（黄彩思）', height=500)
-    radar.set_radar_component(c_schema=schema)
-    radar.add('存活婴儿', [y_list], item_color='#2525f5',symbol=None, area_color='#2525f5', area_opacity=0.3,
+    radar_hcs = Radar('存活婴儿与死亡婴儿数据对比——雷达图(黄彩思)', height=500)
+    radar_hcs.set_radar_component(c_schema=schema_hcs)
+    radar_hcs.add('存活婴儿', [y_mean_hcs], item_color='#2525f5',symbol=None, area_color='#2525f5', area_opacity=0.3,
               legend_top='bottom', line_width=3)
-    radar.add('死亡婴儿', [n_list], item_color="#f9713c",symbol=None, area_color="#ea3a2e", area_opacity=0.3,
+    radar_hcs.add('死亡婴儿', [n_mean_hcs], item_color="#f9713c",symbol=None, area_color="#ea3a2e", area_opacity=0.3,
               legend_top='bottom', line_width=3, legend_text_size=20)
-    radar.render('radar.html')
+    radar_hcs.render('radar.html')
 
 
 def picture_box(file_path):
@@ -77,38 +67,37 @@ def picture_box(file_path):
         绘制箱形图
     :param file_path:数据文件的路径
     """
-    birth = pd.read_csv(file_path)
-    birth_col = birth.loc[:, ['INFANT_ALIVE_AT_REPORT',
+    birth_hcs = pd.read_csv(file_path)
+    birth_col_hcs = birth_hcs.loc[:, ['INFANT_ALIVE_AT_REPORT',
                               'BIRTH_YEAR',
                               'INFANT_WEIGHT_GRAMS']]
     # 取反，去除不需要的数据
-    birth_nien = birth_col[~birth_col['INFANT_WEIGHT_GRAMS'].isin([9999])]
+    birth_nien_hcs = birth_col_hcs[~birth_col_hcs['INFANT_WEIGHT_GRAMS'].isin([9999])]
     # 按年分时间段
-    year_2014 = birth_nien[birth_nien['BIRTH_YEAR'] == 2014]
-    year_2015 = birth_nien[birth_nien['BIRTH_YEAR'] == 2015]
+    year_2014_hcs = birth_nien_hcs[birth_nien_hcs['BIRTH_YEAR'] == 2014]
+    year_2015_hcs = birth_nien_hcs[birth_nien_hcs['BIRTH_YEAR'] == 2015]
     # 再分每年中，存活与死亡的
-    year_2014_y = year_2014[year_2014['INFANT_ALIVE_AT_REPORT'] == 'Y']
-    year_2014_n = year_2014[year_2014['INFANT_ALIVE_AT_REPORT'] == 'N']
-    year_2015_y = year_2015[year_2015['INFANT_ALIVE_AT_REPORT'] == 'Y']
-    year_2015_n = year_2015[year_2015['INFANT_ALIVE_AT_REPORT'] == 'N']
+    year_2014_y_hcs = year_2014_hcs[year_2014_hcs['INFANT_ALIVE_AT_REPORT'] == 'Y']
+    year_2014_n_hcs = year_2014_hcs[year_2014_hcs['INFANT_ALIVE_AT_REPORT'] == 'N']
+    year_2015_y_hcs = year_2015_hcs[year_2015_hcs['INFANT_ALIVE_AT_REPORT'] == 'Y']
+    year_2015_n_hcs = year_2015_hcs[year_2015_hcs['INFANT_ALIVE_AT_REPORT'] == 'N']
     # 实例化一个箱形图对象
-    box = Boxplot('存活婴儿与死亡婴儿数据对比——箱型图（黄彩思）')
+    box_hcs = Boxplot('存活婴儿与死亡婴儿体重对比——箱型图(黄彩思)')
     # 使用自带的prepare_data计算所需的五个数
-    y_data = box.prepare_data([year_2014_y['INFANT_WEIGHT_GRAMS'],
-                               year_2015_y['INFANT_WEIGHT_GRAMS']])
-    n_data = box.prepare_data([year_2014_n['INFANT_WEIGHT_GRAMS'],
-                               year_2015_n['INFANT_WEIGHT_GRAMS']])
+    y_data_hcs = box_hcs.prepare_data([year_2014_y_hcs['INFANT_WEIGHT_GRAMS'],
+                               year_2015_y_hcs['INFANT_WEIGHT_GRAMS']])
+    n_data_hcs = box_hcs.prepare_data([year_2014_n_hcs['INFANT_WEIGHT_GRAMS'],
+                               year_2015_n_hcs['INFANT_WEIGHT_GRAMS']])
     # 获取x_axis轴的数据：每个年度
-    x_axis = birth_nien.drop_duplicates(subset='BIRTH_YEAR')['BIRTH_YEAR'].sort_values()
+    x_axis_hcs = birth_nien_hcs.drop_duplicates(subset='BIRTH_YEAR')['BIRTH_YEAR'].sort_values()
+    print('存活婴儿数据箱型图:', y_data_hcs, '\n', '死亡婴儿数据箱型图:', n_data_hcs)
+    box_hcs.add('存活婴儿', x_axis=x_axis_hcs, y_axis=y_data_hcs)
+    box_hcs.add('死亡婴儿', x_axis=x_axis_hcs, y_axis=n_data_hcs, legend_pos='right')
 
-    box.add('存活婴儿', x_axis=x_axis, y_axis=y_data)
-    box.add('死亡婴儿', x_axis=x_axis, y_axis=n_data, legend_pos='right')
-
-    box.render('box.html')
+    box_hcs.render('box.html')
 
 
 if __name__ == '__main__':
     file_path = '../data/births_train.csv'
-    y, n = get_data(file_path)
-    picture_radar(y, n)
+    picture_radar(file_path)
     picture_box(file_path)
--- Experiments/six.py
+++ Experiments/six.py
@@ -2,54 +2,37 @@ from pyecharts import HeatMap, Map
 import pandas as pd
 
 
-def get_GDP_data(file_path):
-    """
-
-    :param file_path:
-    :return:
-    """
-    # 加载数据，usecols：只使用的列名
-    df = pd.read_csv(file_path, encoding='utf-8', usecols=['地区', '2017年'])
-    province = []
-    value = []
-    for index, col in df.iterrows():
+def picture_map(file_path_hcs):
+    df_hcs = pd.read_csv(file_path_hcs, encoding='utf-8', usecols=['地区', '2017年'])
+    province_hcs = []
+    value_hcs = []
+    for index_hcs, col_hcs in df_hcs.iterrows():
         # 切分地区名     有两个特殊的地区名为三个字
-        if '黑龙' in col[0] or '内蒙' in col[0]:
-            province.append(col[0][:3])
+        if '黑龙' in col_hcs[0] or '内蒙' in col_hcs[0]:
+            province_hcs.append(col_hcs[0][:3])
         else:
-            province.append(col[0][:2])
-        value.append(col[1])
-    return province, value
+            province_hcs.append(col_hcs[0][:2])
+        value_hcs.append(col_hcs[1])
+    map_hcs = Map('全国各地区2017年GDP统计——黄彩思', height=600)
+    map_hcs.add('', province_hcs, value_hcs, is_visualmap=True, visual_range=[1200, 90000], is_label_show=True)
+    map_hcs.render('2017GDP_map.html')
 
 
-def picture_map(province, value):
-    map = Map('全国各地区2017年GDP统计——黄彩思', height=600)
-    map.add('', province, value, is_visualmap=True, visual_range=[1200, 90000], is_label_show=True)
-    map.render('2017GDP_map.html')
-
-
-def get_AQI_data(file_path):
-    df = pd.read_csv(file_path, encoding='utf-8', usecols=['Date', 'AQI'])
-    data = []
-    for index, col in df.iterrows():
-        data.append([col[0],col[1]])
-
-    return data
-
-
-def picture_heatmap(data):
-    hm = HeatMap('广州市2018年的空气质量指数日历热力图——黄彩思')
+def picture_heatmap(file_path_hcs):
+    df_hcs = pd.read_csv(file_path_hcs, encoding='utf-8', usecols=['Date', 'AQI'])
+    data_hcs = []
+    for index_hcs, col_hcs in df_hcs.iterrows():
+        data_hcs.append([col_hcs[0], col_hcs[1]])
+    hm_hcs = HeatMap('广州市2018年的空气质量指数日历热力图——黄彩思')
     # 参照https://www.jianshu.com/p/fc8c78e0df98
-    hm.add('', data, is_calendar_heatmap=True,is_visualmap=True, calendar_date_range='2018',
+    hm_hcs.add('', data_hcs, is_calendar_heatmap=True, is_visualmap=True, calendar_date_range='2018',
            calendar_cell_size=['auto', 30], is_toolbox_show=False, visual_range=[10, 200],
-           visual_split_number=5, is_piecewise=True, visual_orient="horizontal", visual_pos="center",)
-    hm.render('hearmap_AQI.html')
+           visual_split_number=5, is_piecewise=True, visual_orient="horizontal", visual_pos="center", )
+    hm_hcs.render('hearmap_AQI.html')
+
 
 if __name__ == '__main__':
-    # file_GDP_path = '../data/全国各地区GDP统计.csv'
-    # province, value = get_GDP_data(file_GDP_path)
-    # picture_map(province, value)
-    file_AQI_path = '../data/guangzhou_AQI_2018.csv'
-    AQI_data = get_AQI_data(file_AQI_path)
-    picture_heatmap(AQI_data)
-
+    file_GDP_path_hcs = '../data/national_provinces_GDP.csv'
+    picture_map(file_GDP_path_hcs)
+    file_AQI_path_hcs = '../data/guangzhou_AQI_2018.csv'
+    picture_heatmap(file_AQI_path_hcs)
